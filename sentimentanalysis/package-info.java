b5cf982e29f4203dda6f3b3612a3e82ce91c8857
Joseph Mancillas <josephmancillas95@gmail.com>
2023-02-02 19:25:29 -0600

    Delete package-info.java

--- sentimentanalysis/package-info.java
+++
@@ -1,7 +0,0 @@
-/** This package contains classes and methods for sentiment analysis
-    CS 3354 Fall 2018, Texas State University.
-    @author vangelis
-    @author jelena
-    @author junye
-*/
-package sentimentanalysis;
